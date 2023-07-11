b470b2497d73ac127a1ea99d3906cba45444180a
nurs.syhn <nursultan.seyhan@gmail.com>
2023-07-11 13:32:00 +0300

    Initial Commit

---
+++ XsAndOs/XsAndOsApp.swift
@@ -0,0 +1,17 @@
+//
+//  XsAndOsApp.swift
+//  XsAndOs
+//
+//  Created by Nur Sultan Şeyhanlıoğlu on 11.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct XsAndOsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
